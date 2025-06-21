ce84a1542f9b6600c1c6b232c57cbb1904c9df60
imah rahim <imahrahim@MacBook-Pro-imah.local>
2025-06-22 00:07:18 +0800

    Initial commit

---
+++ src/utils/coverImageBySection.ts
@@ -0,0 +1,21 @@
+import CoverBeverages from '../assets/pics/cover/CoverBeverages.png';
+import CoverBreakfast from '../assets/pics/cover/CoverBreakfast.png';
+import CoverDessert from '../assets/pics/cover/CoverDessert.png';
+import CoverKerabu from '../assets/pics/cover/CoverKerabu.png';
+import CoverLala from '../assets/pics/cover/CoverLala.png';
+import CoverRice from '../assets/pics/cover/CoverRice.png';
+import CoverSayur from '../assets/pics/cover/CoverSayur.png';
+import CoverSiakap from '../assets/pics/cover/CoverSiakap.png';
+import CoverSnacks from '../assets/pics/cover/CoverSnacks.png';
+
+export const coverImageBySection: Record<string, string> = {
+  "Beverages": CoverBeverages,
+  "All Day Breakfast": CoverBreakfast,
+  "Dessert": CoverDessert,
+  "Kerabu": CoverKerabu,
+  "Lala": CoverLala,
+  "Rice": CoverRice,
+  "Sayur": CoverSayur,
+  "Siakap": CoverSiakap,
+  "Snacks": CoverSnacks
+};
